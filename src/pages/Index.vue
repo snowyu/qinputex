3bce12f004eb581b3f32234c4a85ffaf47b4b6ed
Riceball LEE <snowyu.lee@gmail.com>
2019-12-25 22:25:12 +0800

    chore: minor change

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -17,6 +17,8 @@
               q-btn(label="append")
             template(v-slot:top)
               .q-gutter-xs
+                q-chip(@click="doPrevClick" clickable color="primary" text-color="white" icon="prev")
+                  | Prev
                 q-chip(removable="" @click="doNextClick" clickable @remove="log('Icecream')" color="primary" text-color="white" icon="cake")
                   | On
                 q-chip(removable="" color="teal" text-color="white" icon="cake")
@@ -25,28 +27,51 @@
                   | Top
                 q-chip(disable="" removable="" color="red" text-color="white" icon="cake")
                   | Slot
+                q-chip(@click="doNextClick" clickable color="primary" text-color="white" icon="next")
+                  | Next
 
         q-item-section(side)
           q-btn(@click="doNextClick" label="next")
       q-item
         q-item-section
+          q-input-ex(type="datetime" value="2012-01-01T12:33:22" @input="log('dt=', $event)")
+      q-item
+        q-item-section
           q-input-ex(ref="custom" :type=`{
             name: 'custom',
-            type: 'text',
+            caption: 'date',
+            mask: '####-##-##T##:##:##Z',
+            type: 'tel',
             attaches: {
-              prepend: {
-                icon: 'event',
-                popup: {
-                  name: 'QDate',
-                  // attrs: {
-                  //   'default-year-month': getCurrentYM()
-                  // }
+              prepend: [
+                {
+                  icon: 'event',
+                  popup: {
+                    name: 'QDate',
+                    'caption': 'date',
+                    attrs: {
+                      mask: 'YYYY-MM-DDTHH:mm:ssZ',
+                    //   'default-year-month': getCurrentYM()
+                    }
+                  },
                 },
-              },
+                {
+                  icon: 'access_time',
+                  popup: {
+                    'name': 'QTime',
+                    'caption': 'time',
+                    attrs: {
+                      mask: 'YYYY-MM-DDTHH:mm:ssZ',
+                      format24h: true,
+                    }
+                  },
+                }
+              ],
               append: {
                 icon: 'colorize',
                 popup: {
                   name: 'QColor',
+                  'caption': 'color',
                 },
               },
             },
@@ -88,8 +113,15 @@ export default {
     }
   },
   methods: {
+    log(){console.log.apply(console, arguments)},
     onInput: (v)=>{
       console.log('qExt',v)
+    },
+    doPrevClick: function() {
+      this.myValue = '';
+      curr--;
+      if (curr < 0) curr =  InputType.length - 1;
+      this.type = InputType[curr];
     },
     doNextClick: function() {
       this.myValue = '';
